63fee5436801427cfe49c31481f31dc270adc4f9
hasagi <hasagi1234@protonmail.com>
2024-12-09 21:08:40 +0100

    Initial commit and last

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace Calculator;
+
+public class Calculator
+{
+    private static double pi = 3.1415926;
+    public string expression { get; set; } 
+    public float result { get; set; }
+    private static string historyPath= @"c:\\Users\\Sabina\\RiderProjects\\Calculator\\Calculator\\history.json";
+
+
+    private static readonly Lazy<Calculator> lazy =
+        new Lazy<Calculator>(() => new Calculator());
+
+    public static Calculator Instance
+    {
+        get { return lazy.Value; }
+    }
+
+    private Calculator()
+    {
+    }
+
+    public float calculate(string expression)
+    {
+        float res = 1;
+        this.expression = expression;
+        expression = expression.Replace(" ","");
+        var exprList = new List<string>();
+        int digitLength = 0;
+        for (int i = 0; i < expression.Length; i++)
+        {
+            if (char.IsDigit(expression[i])|| expression[i] == '.')
+            {
+                digitLength++;
+            }
+            else
+            {
+                exprList.Add(expression.Substring(i-digitLength,i));
+                exprList.Add(expression[i].ToString());
+                digitLength = 0;
+            }
+        }
+        exprList.Add(expression.Substring(expression.Length-digitLength,digitLength));
+        switch (exprList[1])
+        {
+            case "+":
+                res = float.Parse(exprList[0]) + float.Parse(exprList[2]);
+                break;
+            case "-":
+                res = float.Parse(exprList[0]) - float.Parse(exprList[2]);
+                break;
+            case "/":
+                res = float.Parse(exprList[0]) / float.Parse(exprList[2]);
+                break;
+            case "*":
+                res = float.Parse(exprList[0]) * float.Parse(exprList[2]);
+                break;
+        }
+
+        this.result = res;
+        return res;
+    }
+    public double sqrt (string numbe)
+    {
+        this.expression = "Sqrt("+ numbe+")";
+        double numbah = Math.Sqrt(double.Parse(numbe));
+        this.result = (float)numbah;
+        return numbah;
+    }
+
+    public void addHistory()
+    {
+        string jsonStringified = JsonSerializer.Serialize(this, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        });
+        // Ensure file exists
+                                
+        if (!File.Exists(historyPath)||new FileInfo(historyPath).Length ==0)
+        {
+            File.WriteAllText(historyPath, "["+jsonStringified+"]"); // Initialize with an empty JSON array
+                                    
+        }else {
+            File.WriteAllText(historyPath,
+                File.ReadAllText(historyPath).Remove(File.ReadAllText(historyPath).Length - 1) +
+                "," + jsonStringified + "]");
+        }   
+    }
+
+    public string readHistory()
+    {
+        Console.WriteLine("History:");
+        string historyString = (File.ReadAllText(historyPath));
+        string[] historyArr = historyString.Split('"');
+        int serial=1;
+        for (int i = 3; i < historyArr.Length-6; i+=6)
+        {
+            Console.Write(serial+". "+historyArr[i]+"="+historyArr[i+3][2..^7]);
+            serial++;
+        }
+        Console.Write(serial+". "+historyArr[^4]+"="+historyArr[^1][2..^2]+"\n");
+        return "aa";
+    }
+
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator=Calculator.Instance;
+            string userX = "0";
+            while (userX!="4")
+            {
+                Console.WriteLine("Calculator Menu:\r\n 1. Perform Basic Calculation \r\n 2. Calculate Square Root\r\n 3. View History\r\n 4. Exit\r\n");
+                userX = Console.ReadLine();
+                switch (userX)
+                {
+                    case "1":
+                        while (true)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter your calculation, number (+,-,/,*) number. exit to return");
+                                string userExpression = Console.ReadLine();
+                                if (userExpression == "exit")
+                                {
+                                    break;
+                                }
+
+                                calculator.calculate(userExpression);
+                                // because c# doesnt allow escaping characters, we need to set options as relaxed
+                                calculator.addHistory();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Bad input");
+                            }
+                        }
+                        break;
+                    case "2":
+                        while (true)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter your number to Square Root of, exit to return:");
+                                string userExpression = Console.ReadLine();
+                                if (userExpression == "exit")
+                                {
+                                    break;
+                                }
+                                calculator.sqrt(userExpression);
+                                calculator.addHistory();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Bad input");
+                            }
+                        }
+                        break;
+                    case "3":
+                        calculator.readHistory();
+                        break;
+                    case "4":
+                        break;
+                }
+            }
+            
+            
+            Console.WriteLine("byebye");
+        }
+    }
+}
